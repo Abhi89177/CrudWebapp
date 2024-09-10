e00e04832139dc0d871d6692316c5683452113a3
Abhishekyadav2272002@gmail.com <Abhishek Yadav>
2024-09-11 02:21:25 +0530

    Add project files.

---
+++ CrudWebapp/Models/CollegeInfoContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudWebapp.Models;
+
+public partial class CollegeInfoContext : DbContext
+{
+    public CollegeInfoContext()
+    {
+    }
+
+    public CollegeInfoContext(DbContextOptions<CollegeInfoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.StudentBranch)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Student_Branch");
+            entity.Property(e => e.StudentMobile)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Student_Mobile");
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Student_Name");
+            entity.Property(e => e.StudentSection)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("Student_Section");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CrudWebapp/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudWebapp.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public string StudentBranch { get; set; } = null!;
+
+    public string StudentSection { get; set; } = null!;
+
+    public string StudentMobile { get; set; } = null!;
+}
